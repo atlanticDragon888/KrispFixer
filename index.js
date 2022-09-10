dd8793eefc71047746d02f57cadd463e53f5c47b
atlanticDragon888 <zeuslabs888@gmail.com>
2022-09-10 23:34:36 +0800

    Fix: Stupidity

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const { Plugin } = require("powercord/entities");
-const { getModule } = require ("powercord/webpack");
+const { getModule } = require("powercord/webpack");
 module.exports = class PluginName extends Plugin {
   startPlugin() {
-    getModule(['getNoiseCancellation'], false).addChangeListener(()=>{
-        if(getModule(['getNoiseCancellation'], false).getNoiseCancellation() === false && getModule(['getNoiseSuppression'], false).getNoiseSuppression() === true {getModule(['setNoiseSuppression'], false).setNoiseSuppression(false)}
-    }
-    ) 
+    getModule(['getNoiseCancellation'], false).addChangeListener(() => {
+      if (getModule(['getNoiseCancellation'], false).getNoiseCancellation() === false && getModule(['getNoiseSuppression'], false).getNoiseSuppression() === true ){ getModule(['setNoiseSuppression'], false).setNoiseSuppression(false) }
+    })
   }
 }
