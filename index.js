ef84b759ba7558104eb4174c7dd656b380a26f0c
atlanticDragon888 <zeuslabs888@gmail.com>
2022-09-10 23:50:26 +0800

    🧹cleaned up

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const { Plugin } = require("powercord/entities");
 const { getModule } = require("powercord/webpack");
+
 module.exports = class PluginName extends Plugin {
-  startPlugin() {
-    getModule(['getNoiseCancellation'], false).addChangeListener(() => {
-      if (getModule(['getNoiseCancellation'], false).getNoiseCancellation() === false && getModule(['getNoiseSuppression'], false).getNoiseSuppression() === true ){ getModule(['setNoiseSuppression'], false).setNoiseSuppression(false) }
-    })
-  }
-  pluginWillUnload () {
-  }
+	startPlugin() {
+		let allowChange = true;
+
+		// Run whenever Krisp is changed
+		getModule(['getNoiseCancellation'], false).addChangeListener(() => {
+			if (
+				getModule(['getNoiseCancellation'], false).getNoiseCancellation() === false &&
+				getModule(['getNoiseSuppression'], false).getNoiseSuppression() === true &&
+				allowChange
+			) {
+				// Disable noise suppression once
+				getModule(['setNoiseSuppression'], false).setNoiseSuppression(false);
+				allowChange = false
+			} else if (getModule(['getNoiseCancellation'], false).getNoiseCancellation() === true) {
+				// Krisp is back on, allow changes again
+				allowChange = true
+			}
+		})
+	}
+	pluginWillUnload() { }
 }
